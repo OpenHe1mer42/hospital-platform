0e613a06db8c84ca39c403fb64d7a5b03ae7e1ee
JonZylfiu <jonzylfiu5@gmail.com>
2026-05-02 18:41:38 +0200

    Implemented diagnosis response dto

--- src/main/java/dto/diagnosis/DiagnosisResponseDTO.java
+++ src/main/java/dto/diagnosis/DiagnosisResponseDTO.java
@@ -1,5 +1,59 @@
 package dto.diagnosis;
 
-public class DiagnosisResponseDTO {
+import java.time.Instant;
 
+public class DiagnosisResponseDTO {
+    private Integer patientId;
+    private Integer doctorId;
+    private String prescription;
+    Instant createdAt;
+    Instant updatedAt;
+
+    public DiagnosisResponseDTO(Integer patientId, Integer doctorId, String prescription, Instant createdAt, Instant updatedAt) {
+        this.patientId = patientId;
+        this.doctorId = doctorId;
+        this.prescription = prescription;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Integer getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+
+    public Integer getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(int doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public String getPrescription() {
+        return prescription;
+    }
+
+    public void setPrescription(String prescription) {
+        this.prescription = prescription;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
